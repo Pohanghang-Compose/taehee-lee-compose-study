d411910a2c2d7a3f6c2d95e25d5c0a1b43a716a9
HaeTi <t9h9@naver.com>
2024-03-13 04:32:58 +0900

    activity setting

--- app/src/main/java/com/haeti/sopose/MainActivity.kt
+++ app/src/main/java/com/haeti/sopose/MainActivity.kt
@@ -3,6 +3,7 @@ package com.haeti.sopose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -11,16 +12,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
+import com.haeti.sopose.auth.AuthViewModel
 import com.haeti.sopose.navigation.MainNavHost
 import com.haeti.sopose.ui.theme.SoposeTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val authViewModel: AuthViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SoposeTheme {
                 val navController = rememberNavController()
-                MainNavHost(navController = navController)
+                MainNavHost(navController = navController, viewModel = authViewModel)
             }
         }
     }
