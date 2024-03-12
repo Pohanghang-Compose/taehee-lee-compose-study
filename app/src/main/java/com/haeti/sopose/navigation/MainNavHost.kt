8ad9457ca79ed33643f96e8e0fee3ae559e8cc56
HaeTi <t9h9@naver.com>
2024-03-13 04:31:29 +0900

    screen에서 navController와 viewmodel을 전달받도록 변경

--- app/src/main/java/com/haeti/sopose/navigation/MainNavHost.kt
+++ app/src/main/java/com/haeti/sopose/navigation/MainNavHost.kt
@@ -4,26 +4,28 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.haeti.sopose.auth.AuthViewModel
 import com.haeti.sopose.login.LoginScreen
 import com.haeti.sopose.main.MainScreen
 import com.haeti.sopose.signup.SignUpScreen
 
 @Composable
 fun MainNavHost(
-    navController: NavHostController
+    navController: NavHostController,
+    viewModel: AuthViewModel
 ) {
     NavHost(
         navController = navController,
         startDestination = Screen.Login.route
     ) {
         composable(Screen.Login.route) {
-            LoginScreen()
+            LoginScreen(navController = navController, authViewModel = viewModel)
         }
         composable(Screen.SignUp.route) {
-            SignUpScreen()
+            SignUpScreen(navController = navController, authViewModel = viewModel)
         }
         composable(Screen.Main.route) {
-            MainScreen()
+            MainScreen(authViewModel = viewModel)
         }
     }
 }
