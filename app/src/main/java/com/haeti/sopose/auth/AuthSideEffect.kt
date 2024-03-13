98f4256a03f612465de14b52a4f9be1d5ffe96b2
HaeTi <t9h9@naver.com>
2024-03-13 22:49:12 +0900

    data object

--- app/src/main/java/com/haeti/sopose/auth/AuthSideEffect.kt
+++ app/src/main/java/com/haeti/sopose/auth/AuthSideEffect.kt
@@ -1,9 +1,8 @@
 package com.haeti.sopose.auth
 
 sealed class AuthSideEffect {
-    object LoginSuccess: AuthSideEffect()
-    object InvalidInputToast: AuthSideEffect()
-    object SignUpSuccess: AuthSideEffect()
-    object NavigateToSignUp: AuthSideEffect()
-
+    data object LoginSuccess : AuthSideEffect()
+    data object InvalidInputToast : AuthSideEffect()
+    data object SignUpSuccess : AuthSideEffect()
+    data object NavigateToSignUp : AuthSideEffect()
 }
